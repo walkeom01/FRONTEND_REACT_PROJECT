e24e2bc5e67e08fb8c36b4ab476e054332b6ace8
walkeom01 <walkeom860@gmail.com>
2026-03-30 21:52:55 +0530

    navbar completed

--- ELECTRONICS_ECOMMERCE/src/components/Navbar/Navbar.jsx
+++ ELECTRONICS_ECOMMERCE/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { IoMdSearch } from "react-icons/io";
+
+const MenuLinks = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Shop",
+        link: "/#shop",
+    },
+    {
+        id: 3,
+        name: "About",
+        link: "/#about",
+    },
+    {
+        id: 4,
+        name: "Blogs",
+        link: "/blogs",
+    },
+
+]
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
-                <div className='container '>
-                    <div>
-                        <a href="#" className='text-primary  font-semibold   ' >Eshop</a>
+        <div className='bg-white dark:bg-gray-800 dark:text-white duration-200 relative z-40'>
+            <div className='py-4'>
+                <div className='container flex justify-between items-center '>
+                    <div className='flex items-center gap-4'>
+                        <a href="#" className='text-primary  font-semibold  tracking-widest text-2xl uppercase sm:text-3xl  '>Eshop</a>
+
+                        <div className='hidden lg:block'>
+                            <ul className='flex items-center gap-4 '>
+                                {
+                                    MenuLinks.map((data, index) => (
+                                        <li key={index}>
+                                            <a href={data.link}
+                                                className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'>
+                                                {""}
+                                                {data.name}</a>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    {/*navbar right section */}
+                    <div className='flex justify-between item-center gap-4 '>
+                        {/* Search Bar Section */}
+                        <div className='realative group hidden sm:block'>
+                            <input type="text"
+                                placeholder='Search' className='
+                                search-bar 
+                                '/>
+                            <IoMdSearch
+                                className='text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2  right -3 '
+                            />
+                        </div>
+
+                        {/* dark mode Section */}
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
