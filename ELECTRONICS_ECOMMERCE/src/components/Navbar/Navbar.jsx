0b1f97ba2e7c683a6d9b749c44d3b2f33c333118
walkeom01 <walkeom860@gmail.com>
2026-03-31 10:02:43 +0530

    -> completed dark mode feature to whole page -> added navbar dropdown links using tailwindcss

--- ELECTRONICS_ECOMMERCE/src/components/Navbar/Navbar.jsx
+++ ELECTRONICS_ECOMMERCE/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa6"
+import DarkMode from '../DarkMode';
 
 const MenuLinks = [
     {
@@ -25,6 +28,25 @@ const MenuLinks = [
 
 ]
 
+const DropDownLinks = [
+    {
+        id: 1,
+        name: "Trending Products",
+        link: "/#",
+    },
+
+    {
+        id: 2,
+        name: "Best Selling",
+        link: "/#",
+    },
+
+    {
+        id: 3,
+        name: "Top Rated",
+        link: "/#"
+    },
+]
 const Navbar = () => {
     return (
         <div className='bg-white dark:bg-gray-800 dark:text-white duration-200 relative z-40'>
@@ -45,23 +67,53 @@ const Navbar = () => {
                                         </li>
                                     ))
                                 }
+                                {/* Dropdown */}
+                                <li className='relative cursor-pointer group '>
+                                    <a href="#" className='flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2'>
+                                        Quick Links
+                                        <span>
+                                            <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                                        </span>
+                                    </a>
+                                    {/* dropdown links */}
+                                    <div className='absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white'>
+                                        <ul className='space-y-2'>
+                                            {
+                                                DropDownLinks.map((data, index) => (
+                                                    <li >
+                                                        <a className='rounded-md font-semibold text-gray-500 dark:hover:text-white duration-300 inline-block w-full p-2 hover:bg-primary/20' href={data.link}>{data.name}</a>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+                                </li>
                             </ul>
                         </div>
                     </div>
                     {/*navbar right section */}
-                    <div className='flex justify-between item-center gap-4 '>
+                    <div className='flex justify-between items-center gap-4 '>
                         {/* Search Bar Section */}
-                        <div className='realative group hidden sm:block'>
+                        <div className='relative group hidden sm:block'>
                             <input type="text"
                                 placeholder='Search' className='
                                 search-bar 
                                 '/>
                             <IoMdSearch
-                                className='text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2  right -3 '
+                                className='text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2  right-3 '
                             />
                         </div>
-
+                        {/* order-button */}
+                        <button className='relative p-3'>
+                            <FaCartShopping className='text-xl text-gray-600 ' />
+                            <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs  '>
+                                4
+                            </div>
+                        </button>
                         {/* dark mode Section */}
+                        <div className='flex items-center justify-center'>
+                            <DarkMode />
+                        </div>
                     </div>
                 </div>
             </div>
