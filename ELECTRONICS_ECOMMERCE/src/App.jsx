768f42140ea87fc90d51d18ba4b5c695f3efe630
walkeom01 <walkeom860@gmail.com>
2026-04-01 22:27:27 +0530

    card section

--- ELECTRONICS_ECOMMERCE/src/App.jsx
+++ ELECTRONICS_ECOMMERCE/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Hero from './components/Hero/Hero.jsx'
-
+import Category from './components/Category/Category.jsx'
 const App = () => {
   return (
     <div >
       <Navbar />
       <Hero />
+      <Category></Category>
     </div>
   )
 }
