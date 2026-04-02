7819c621052034851745f7c3f2f90f7e23bca1c1
walkeom01 <walkeom860@gmail.com>
2026-04-02 11:04:08 +0530

    banner styling done

--- ELECTRONICS_ECOMMERCE/src/App.jsx
+++ ELECTRONICS_ECOMMERCE/src/App.jsx
@@ -4,7 +4,22 @@ import Hero from './components/Hero/Hero.jsx'
 import Category from './components/Category/Category.jsx'
 import Category2 from './components/Category/Category2.jsx'
 import Services from './components/Services/Services.jsx'
- 
+import Banner from './components/Banner/Banner.jsx';
+import headphone from './assets/hero/headphone.png';
+
+
+const BannerData ={
+  discount : "30%",
+  title : "fine smile",
+  date : "2 april 2026",
+  image : headphone ,
+  title2 :"air solo bass",
+  title3 : "winter sale",
+  title4 : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, exercitationem.",
+  bgColor : "#f42c37"
+   
+
+};
 const App = () => {
   return (
     <div className='dark:bg-gray-800 dark:text-white duration-700 overflow-hidden'>
@@ -13,6 +28,7 @@ const App = () => {
       <Category></Category>
       <Category2></Category2>
       <Services></Services>
+      <Banner data={BannerData}></Banner>
     </div>
   )
 }
